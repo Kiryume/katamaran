ecc14cd7889e0581847dad8367fa8a3a98deb255
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-10-05 04:46:47 +0200

    chore: minor changes in code base

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,8 @@ fn main() {
 
 fn run() -> Result<(), String> {
     let args = Args::parse();
-    let input = std::fs::read_to_string(args.input).expect("Failed to read input file");
+    let input =
+        std::fs::read_to_string(args.input).map_err(|s| "Failed to read input file".to_string())?;
     let mut lexer = lexer::LexerCursor::new(&input);
     let mut tokens = lexer.collect::<Result<Vec<_>, String>>()?;
     let tokenstream = tokentree::TokenTree::parse_from_tokens(&mut tokens.into_iter())?;
